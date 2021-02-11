12d06aa06be84e96bf1cbdb3e9eeefb2a56bcbc3
sagarmakwana17 <77189472+sagarmakwana17@users.noreply.github.com>
2021-02-11 23:34:23 +0530

    database modified

--- database/notesmarketplace.sql
+++ database/notesmarketplace.sql
@@ -1,21 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 11, 2021 at 01:04 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Database: `notesmarketplace`
